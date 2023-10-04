f3672f453ebba4bc35ceb9640583dbe5212ac7ae
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2023-10-04 14:05:16 +0530

    river temp completed app

--- backend/main.py
+++ backend/main.py
@@ -1,57 +1,49 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 import numpy as np
-import pandas as pd
+import joblib
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 app = FastAPI()
 
-# Sample data
-# Sample water temperature data
-water_temp_data = {
-    "Year": [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015],
-    "Jan": [27, 26, 26, 25, 23, 25, 26, 24, 26],
-    # ... (other months)
-    "Dec": [None, 27, 26, 26, 25, 28, 25, 27, 21]
-}
-
-# Model training
-X = air_temp_data
-y = water_temp_df["Jan"].values
-# Model training for Linear Regression
-linear_model = LinearRegression()
-linear_model.fit(X, y)
-
-# Model training for Decision Tree
-tree_model = DecisionTreeRegressor()
-tree_model.fit(X, y)
+# Load the saved models
+best_linear_model = joblib.load('best_linear_model.pkl')
+best_rf_model = joblib.load('best_rf_model.pkl')
 
 class PredictionInput(BaseModel):
     air_temperature: float
+    river_temperature: float
     model_type: str
     metric: str
 
 @app.post("/predict/")
 def predict(input_data: PredictionInput):
     if input_data.model_type == 'Linear Regression':
-        selected_model = linear_model
-    elif input_data.model_type == 'Decision Tree':
-        selected_model = tree_model
+        selected_model = best_linear_model
+    elif input_data.model_type == 'Random Forest':
+        selected_model = best_rf_model
     else:
         return {"error": "Invalid model type"}
 
-    y_pred = selected_model.predict(np.array([[input_data.air_temperature]]))
-    
-    # Dummy observed river temperature for metric calculation
-    observed_river_temp = 27  
+    # Prepare the input as numpy array
+    # Here I'm assuming that the model uses air_temperature for prediction. 
+    # Adjust this as needed for your specific use case.
+    input_array = np.array([[input_data.air_temperature]])
+
+    # Perform prediction
+    y_pred = selected_model.predict(input_array)
+
+    # Use the provided observed river temperature for metric calculation
+    observed_river_temp = input_data.river_temperature
     
     if input_data.metric == 'RMSE':
-        metric_value = mean_squared_error([observed_river_temp], [y_pred], squared=False)
+        metric_value = np.sqrt(mean_squared_error([observed_river_temp], [y_pred]))
     elif input_data.metric == 'MAE':
         metric_value = mean_absolute_error([observed_river_temp], [y_pred])
     elif input_data.metric == 'MSE':
         metric_value = mean_squared_error([observed_river_temp], [y_pred])
-    
+    else:
+        return {"error": "Invalid metric type"}
+
     return {"metric_value": metric_value, "predicted_value": y_pred[0]}
+
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import json
 import requests
+
+# Dummy Data for Demo (Replace these with real source data)
+dummy_air_temp_data = [20, 21, 22, 23, 24, 25]
+dummy_river_temp_data = [18, 19, 20, 21, 22, 23]
 
 def main():
     st.title("River Water Quality Prediction Tool")
@@ -9,28 +15,41 @@ def main():
     if module == "River Water Temperature":
         run_river_water_temperature_module()
 
-# Module for River Water Temperature
 def run_river_water_temperature_module():
     st.header("River Water Temperature Prediction")
 
-    air_temp = st.selectbox("Select Air Temperature", air_temp_data)
-    observed_river_temp = st.selectbox("Select Observed River Temperature", observed_river_temp_data)
-    model_name = st.selectbox("Select Model", ["Linear Regression", "Random Forest"])
+    air_temp = st.selectbox("Select Air Temperature", dummy_air_temp_data)
+    river_temp = st.selectbox("Select Observed River Temperature", dummy_river_temp_data)
+    model_type = st.selectbox("Select Model", ["Linear Regression", "Random Forest"])
     metric = st.selectbox("Select Performance Metric", ["RMSE", "MAE", "MSE"])
 
     if st.button("Submit"):
+        # Prepare the data to send as JSON
         data = {
             "air_temperature": air_temp,
-            "river_water_temperature": observed_river_temp,
-            "model_type": model_name,
+            "river_temperature": river_temp,
+            "model_type": model_type,
             "metric": metric
         }
 
-        response = requests.post("http://localhost:8000/predict/", json=data)
+        # Fetch data from FastAPI backend
+        response = requests.post("http://127.0.0.1:8000/predict/", json=data)
+
         if response.status_code == 200:
             result = response.json()
-            st.write(f"Metric Value: {result['metric_value']}")
             st.write(f"Predicted River Temperature: {result['predicted_value']}")
+
+            # Option to download the output in Excel/CSV
+            download_data = {"Predicted Value": [result['predicted_value']]}
+            df_download = pd.DataFrame(download_data)
+            csv = df_download.to_csv(index=False)
+            st.download_button(
+                label="Download CSV File",
+                data=csv,
+                file_name='predicted_river_temperature.csv',
+                mime='text/csv',
+            )
+
         else:
             st.write("An error occurred during prediction.")
 
